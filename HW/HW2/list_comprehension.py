f011a2ad3f064a47ae22c5a74cdecbd8c15511ca
Benjamin Goldstone <bgoldstone1@gmail.com>
2022-02-05 16:38:05 -0500

    added better formatting to print statements

--- HW/HW2/list_comprehension.py
+++ HW/HW2/list_comprehension.py
@@ -13,8 +13,8 @@ def main():
 
     # cat
     cat = ["cat" for _ in range(500)]
-    print(f'Length: {len(cat)}, cat[0]: {cat[0]}, cat[499]: {cat[499]}')
-
+    print(f'\nLength: {len(cat)}, cat[0]: {cat[0]}, cat[499]: {cat[499]}')
+    # print(str(cat))
     # Countdown
     count = [x for x in range(100, 0, -1)]
     print(f'\nLength: {len(count)}\nCountdown: \n{count}')
@@ -22,13 +22,13 @@ def main():
     # one_hundred_squares
     one_hundred_squares = [[x**2 for x in range(1, 101)] for _ in range(100)]
     print(
-        f'Length: {len(one_hundred_squares)}, one_hundred_squares[0:5]: {one_hundred_squares[0][0:5]}, one_hundred_squares[95:] {one_hundred_squares[99][95:]}')
+        f'\nLength: {len(one_hundred_squares)}, one_hundred_squares[0][0:5]: {one_hundred_squares[0][0:5]}, one_hundred_squares[99][95:] {one_hundred_squares[99][95:]}')
     # print("\n\n" + str(one_hundred_squares))
 
     # word count
     string = "Today is Monday and it is not very hot"
     word_count = [len(word) for word in string.split()]
-    print(word_count)
+    print(f'\nWord letter count for "{string}" is {word_count}\n')
 
 
 if __name__ == "__main__":
