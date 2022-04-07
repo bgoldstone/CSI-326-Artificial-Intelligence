4758564dd6649e7fa61455cee383c8706bb70f8c
Benjamin Goldstone <bgoldstone1@gmail.com>
2022-04-07 16:53:51 -0400

    added total files read

--- HW/HW6/parse_data.py
+++ HW/HW6/parse_data.py
@@ -1,6 +1,7 @@
 import json
 import os
 import re
+import time
 from typing import Dict
 from webbrowser import get
 
@@ -23,22 +24,33 @@ def get_files(directory: str) -> None:
     # Finds Spam Words
     os.chdir(spam_dir)
     spam = {}
+    total_spam_files = 0
+    print("Reading Spam files")
+    time.sleep(1)
     for f in os.listdir(spam_dir):
         with open(f, 'r', errors='ignore') as f:
-            print(f)
+            print(f'Reading {f.name}')
             for word in re.findall(find_by, f.read()):
                 # puts word into spam dictionary.
                 spam[word] = spam.get(word, 0) + 1
-
+        total_spam_files += 1
+    # total spam files read
+    spam['total_spam_files'] = total_spam_files
     # Finds Ham Words
     os.chdir(ham_dir)
     ham = {}
+    print("Reading Ham files")
+    time.sleep(1)
+    total_ham_files = 0
     for f in os.listdir(ham_dir):
         with open(f, 'r', errors='ignore') as f:
-            print(f)
+            print(f'Reading {f.name}')
             for word in re.findall(find_by, f.read()):
                 # puts word into ham dictionary.
                 ham[word] = ham.get(word, 0) + 1
+        total_ham_files += 1
+    # total ham files read
+    ham['total_ham_files'] = total_ham_files
 
     # changes directory to same path as file.
     os.chdir(os.path.dirname(__file__))
