e4628a81094315278c76be78c252e6a16ad70b5a
Benjamin Goldstone <bgoldstone1@gmail.com>
2022-04-18 21:39:50 -0400

    added comments to HW7 scrape.py

--- HW/HW7/main.py
+++ HW/HW7/main.py
@@ -1,10 +1,21 @@
 from scrape import scrape_data
+import os
+import re
 
 
 def main():
+    """
+    main Main function for HW7.
+    """    
     urls = scrape_data("http://www.muhlenberg.edu/")
-    for url in urls:
-        print(url)
+    # os.chdir(os.path.join(os.path.dirname(__file__), "output"))
+    # for index, url in enumerate(urls):
+    #     filename = f'URL{index}.txt'
+    #     with open(filename, 'w') as f:
+    #         f.write(f'URL: {url[0]}\n')
+    #         f.write(f'Relative Links: {url[1]}\n')
+    #         f.write(f'Absolute Links: {url[2]}\n')
+    #         f.write(f'List of Absolute Links: {url[4]}\n')
 
 
 if __name__ == "__main__":
--- HW/HW7/scrape.py
+++ HW/HW7/scrape.py
@@ -1,54 +1,110 @@
+import copy
 import re
-import sys
+from typing import List, Union
 from queue import LifoQueue
 
 import requests
 
+# contants
+# regex for link and anchor tags. group 0 is rel, group 1 is absolute.
+LINK_REGEX = re.compile(r'<link rel=.*href=\"(.+)\"|<a.*href="([^ ]*)"')
 
-def scrape_data(first_url: str):
+
+def add_forward_slash(url: str) -> str:
+    """
+        add_forward_slash Adds forward slash to url if not already present.
+
+        Args:
+            url (str): URL to check
+
+        Returns:
+            str: url with forward slash
+        """
+    return url if url[-1] == '/' else f'{url}/'
+
+
+def scrape_data(first_url: str) -> List[List]:
+    """
+    scrape_data Scrapes the first 500 urls.
+
+    Args:
+        first_url (str): The url to start the web scrape from
+
+    Returns:
+        List[List]: list of each url in the following format: [url, # relative links(<link>), # anchor links(<a>), html contents, [**absolute links]].
+    """
+    # keeps track of visited urls.
     visited = set()
+    # return value list.
     return_value = []
-    stack = LifoQueue(501)
-    first_url = first_url if first_url[-1] == '/' else f'{first_url}/'
-    root_url = re.findall(r'\.([\w\d]+\.[\w\d]{3})', first_url)[0]
+    # stack for DFS of urls.
+    stack = LifoQueue()
+    # first url to start the web scrape from.
+    first_url = add_forward_slash(first_url)
+    # gets domain and top level domain of url. Ex. 'example.com'
+    domain = re.findall(r"https?:\/\/w*\.?([\w]+\.[\w]+)", first_url)[0]
+    # gets full root url Ex. 'http://www.example.com/'
+    root_url = f'http://www.{domain}/'
+    # puts first url in stack to start.
     stack.put(first_url)
+    # scrape until 500 urls are scraped.
     while(len(visited) <= 500):
         stack_url = stack.get()
-        stack_url = stack_url if stack_url[-1] == '/' else f'{stack_url}/'
-        print(f'{len(visited)} {stack_url}')
+        # if url not visited, scrape the url.
         if stack_url not in visited:
-            current_url = get_url(stack_url)
-            return_value.append(current_url[0:4])
-            visited.add(stack_url)
-            for url in current_url[4]:
-                if url not in visited and root_url in url and (url.startswith("http") or url.startswith("/")):
-                    stack.put(url)
+            print(
+                f'Visited:{len(visited)} Stack: {stack.qsize()} URL: {stack_url}')
+            # gets the url scraping.
+            current_url = get_url(stack_url, root_url)
+            # checks if urls was successfully visited.
+            if current_url:
+                # adds url to return list and visited stack.
+                return_value.append(current_url)
+                visited.add(stack_url)
+                # for all of the absolute links, add them to the stack.
+                for url in current_url[4]:
+                    # makes sure urls is not visited, the url is part of current domain, and url is a absolute url.
+                    if url not in visited and (domain in url and url.startswith("http")):
+                        stack.put(url)
     return return_value
 
 
-def get_url(base_url: str):
-    url = ''
+def get_url(base_url: str, root_url: str) -> Union[List, bool]:
+    """
+    get_url Scrapes teh url and returns a list of its findings.
+
+    Args:
+        base_url (str): URL to check.
+        root_url (str): Root url of the domain.
+
+    Returns:
+        Union[List, bool]: URL findings in the format of [url, # relative links(<link>), # anchor links(<a>), html contents, [**absolute links]]. Returns False if url not successfully reachable.
+    """
+
+    # if url fails to resolve, return blank list.
     try:
         url = requests.get(base_url)
     except requests.exceptions.ConnectionError:
         print("site is not reachable", base_url)
-        return [base_url, 0, 0, "", []]
-    # 0 is rel, 1 is absolute
-    link_regex = re.compile(r'<link rel=.*href=\"(.+)\"|<a.*href="([^ ]*)"')
-    # url, number of relative links, number of absolute links, html contents, list of links
+        return False
+    # sets html contents index.
+    # url, number of relative links, number of anchor links, html contents, list of links.
     return_val = [base_url, 0, 0, url.text, []]
-    if url.status_code == 200:
-        for match in link_regex.findall(url.text):
-            if match[0]:
-                return_val[1] += 1
-            elif match[1]:
-                current_match = str(match[1])
-                if "#" in match[1]:
-                    current_match = current_match[:current_match.find("#")]
-
-                return_val[2] += 1
-                if current_match.find("#") == 0:
-                    continue
-                return_val[4].append(current_match if not (current_match.startswith(
-                    '/') or current_match == "") else f'{base_url}{current_match[1:]}')
-    return return_val
+    return_val[3] = url.text
+    # if url is successfully retrieved, get matches from regular expressions.
+    for match in LINK_REGEX.findall(url.text):
+        # if relative link.
+        if match[0]:
+            return_val[1] += 1
+        # if absolute link.
+        elif match[1]:
+            current_match = str(match[1])
+            # if id, ignore up to id.
+            current_match = current_match[:current_match.find(
+                "#")] if "#" in match[1] else current_match
+            return_val[2] += 1
+            # add to list of links if not relative link, or blank, else return root_url + (blank or relative url)
+            return_val[4].append(current_match if not (current_match.startswith(
+                '/') or current_match == "") else f'{root_url}{current_match[1:]}')
+    # returns return_val if successful request.
+    return return_val if url.status_code == 200 else False
